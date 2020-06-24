a66e2426721d8444be7b34909e480088dc80d552
godspeednaomi <godspeednaomi@gmail.com>
2020-06-24 08:41:04 +0800

    Update app.js

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -14,6 +14,8 @@ roundScore = 0;
 activePlayer = 0;
 dice = Math.floor(Math.random() * 6 ) + 1;
 console.log(dice);
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<H1>' + dice + '</H1>';
-document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('#current-'+ activePlayer ).innerHTML = '<em>' + dice + '</em>';
+var x  = document.querySelector('#score-0').textContent;
+console.log(x);
+document.querySelector('.dice').style.display = 'none'
