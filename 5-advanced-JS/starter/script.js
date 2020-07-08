9d449414bf131e7a97a45a8f38b690112dc8ad91
godspeednaomi <godspeednaomi@gmail.com>
2020-07-08 16:45:28 +0800

    Update script.js

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,15 +1,11 @@
+var add = (function(){
+var counter = 0;
+  return function (){
+      return counter+=1;
+  }
+})();
 
-function Person(name,age){
-  let person={};
-  person.name = name;
-  person.age = age;
+console.log(add());
 
-person.calculateAge = function(){
-  console.log(`${this.name}:2020-${this.age}`);
-}
-return person;
-}
-var p1 = Person('ray',3);
-console.log(p1.calculateAge());
-var p2 = Person('bruce',7);
-console.log(p2.calculateAge());
+console.log(add());
+console.log(add());
